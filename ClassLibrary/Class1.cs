d36d5117e929bbcb0c8ef5f952bb15aaeefbc3a0
MileninaElizaveta <159778043+MileninaElizaveta@users.noreply.github.com>
2024-11-06 11:49:39 +0300

    Initial commit

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Circle
+    {
+        public double r;
+        public Circle() { }
+
+        public Circle(double r)
+        {
+            double radius = r;
+        }
+
+        public double Area(double radius)
+        {
+            double v = Math.PI * Math.Pow(r,2);
+            return v;
+        }
+
+        public double Cirle(double radius)
+        {
+            double P = 2 * Math.PI * r;
+            return P;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TesArea()
+        {
+            double radius = 6;
+            double expected = 113;
+            Circle rc = new Circle();
+            double actual = rc.Area(radius);
+            Assert.AreEqual(expected, actual, "Площадь рассчитывается не правильно.");
+        }
+    }
+    [TestClass]
+    public class UnitTest2
+    { 
+        [TestMethod]
+        public void TetCirle()
+        {
+            double radius = 15;
+            double expected = 94;
+            Circle rc = new Circle();
+            double actual = rc.Cirle(radius);
+            Assert.AreEqual(expected, actual, "Окружность рассчитывается не правильно.");
+        }
+    }
+}
